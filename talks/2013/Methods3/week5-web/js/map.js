96c551a98c953751dc6adf6456d95f57aaaa8227
Eric Brelsford <ebrelsford@gmail.com>
2013-09-23 11:14:26 -0400

    Host geojson in pages

--- talks/2013/Methods3/week5-web/js/map.js
+++ talks/2013/Methods3/week5-web/js/map.js
@@ -9,8 +9,9 @@ function initializeMap() {
     var toner = new L.StamenTileLayer('toner');
     map.addLayer(toner);
 
-    $.get('https://gist.github.com/ebrelsford/6478671/raw/b1e87041fff5108f5b1ea06d72da6f2cf8672210/citibike.geojson', function (data) {
-        var data = L.geoJson(data, {
+    $.get('citibike.geojson', function (data) {
+        console.log(data);
+        var layer = L.geoJson(data, {
             style: {
                 color: '#FF0000',
                 fillOpacity: 0.8
